ed6fbd91ff4feff4c4530612f173051e75381f58
Leran <leran158@gmail.com>
2019-09-30 17:28:39 +0000

    change port again

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000 || process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("The YelpCamp Server Has Started!");
 });
